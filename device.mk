052d2a37274f4ee8997e93486a09241dd4d7fb74
vansh123456 <vanshbhatnagar13@gmail.com>
2020-10-20 19:31:14 +0530

    fix build errors

--- device.mk
+++ device.mk
@@ -366,7 +366,9 @@ PRODUCT_USES_QCOM_HARDWARE := true
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
