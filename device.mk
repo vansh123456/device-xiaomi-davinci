2edafad2036321223d28c53cd3b322a93cb13aa8
vansh123456 <vanshbhatnagar13@gmail.com>
2020-10-26 11:37:54 +0530

    enable blur

--- device.mk
+++ device.mk
@@ -98,6 +98,14 @@ PRODUCT_COPY_FILES += \
 #   android.hardware.bluetooth.a2dp@1.0-impl \
 #    android.hardware.bluetooth.a2dp@1.0-service
 
+
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1
+    ro.surface_flinger.supports_background_blur=1
+    debug.sf.latch_unsignaled=1
+    debug.sf.disable_backpressure=1
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti
