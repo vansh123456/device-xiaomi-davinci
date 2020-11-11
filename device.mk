9ac730a94ea64038b603a98f5eca35903b9135bb
vansh123456 <vanshbhatnagar13@gmail.com>
2020-11-11 05:53:43 +0530

    Revert "raphael: Build GoogleCamera app"
    This reverts commit 2fb1621b8ab9975080ba9d4b5a0bfa62403f3ee2.

--- device.mk
+++ device.mk
@@ -130,10 +130,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
-# GoogleCamera
-PRODUCT_PACKAGES += \
-    GoogleCamera
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
