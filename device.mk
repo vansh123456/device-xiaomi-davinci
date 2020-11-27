917653bcdb17bf64e386ffee5d18bb3e6457d94e
Vansh123456 <vanshbhatnagar13@gmail.com>
2020-11-27 14:33:15 +0000

    some derp fixes

--- device.mk
+++ device.mk
@@ -304,7 +304,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/kangos-overlay
+    $(LOCAL_PATH)/overlay-kangos
 
 # Power
 PRODUCT_PACKAGES += \
