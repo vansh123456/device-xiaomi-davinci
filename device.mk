e8bf762493b2bd037826c5002dad665322bcac76
vansh123456 <vanshbhatnagar13@gmail.com>
2020-12-07 16:35:58 +0530

    Revert "fix build errors"
    This reverts commit 052d2a37274f4ee8997e93486a09241dd4d7fb74.

--- device.mk
+++ device.mk
@@ -369,9 +369,8 @@ PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf \
     hardware/qcom-caf/sm8150
+
 
 # Telephony
 PRODUCT_PACKAGES += \
