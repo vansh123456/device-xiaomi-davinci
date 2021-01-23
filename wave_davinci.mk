d290fe106df762b4b8f2238aa5a8c034092dc297
vansh123456 <vanshbhatnagar13@gmail.com>
2021-01-23 12:08:54 +0530

    Davinci:More Changes Required for WaveOS Switch to permissive remove Livedisplay deps remove Previously added redfin spoofs

--- wave_davinci.mk
+++ wave_davinci.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/wave/config/common_full_phone.mk)
+$(call inherit-product, vendor/wave/configs/common.mk)
 
 # Inherit from MiuiCamera vendor
 $(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
@@ -32,5 +32,7 @@ PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
