74cae092220c33b36885524a4a13b8ef1473d555
vansh123456 <vanshbhatnagar13@gmail,com>
2021-10-07 10:27:30 +0000

    posp bringup

--- potato_davinci.mk
+++ potato_davinci.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/potato/config/common.mk)
 
-PRODUCT_NAME := revengeos_davinci
+PRODUCT_NAME := potato_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
